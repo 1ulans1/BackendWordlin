8c11643cb733a1052e5b46086807c4b1f5e7fbaf
maksimblankov <maxblankov436@gmail.com>
2022-10-12 09:39:47 +0300

    add WordTranslationEntity

---
+++ src/main/kotlin/com/wordlin/backendwordlin/entity/WordTranslationEntity.kt
@@ -0,0 +1,20 @@
+package com.wordlin.backendwordlin.entity
+
+import javax.persistence.*
+
+@Entity
+data class WordTranslationEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long,
+    @OneToOne
+    var word: WordEntity,
+
+    @ManyToMany(cascade = [CascadeType.ALL])
+    @JoinTable(
+        name = "word_translate",
+        joinColumns = [JoinColumn(name = "word_id")],
+        inverseJoinColumns = [JoinColumn(name = "translation_id")]
+    )
+    var translation: Set<WordEntity> = HashSet()
+)
